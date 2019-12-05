db0e4c9f3b73268d36f556b7696953f8bdabedb0
Valera1978 <valera1978@tut.by>
2019-12-05 19:57:47 +0300

    scsi: ufs: fix race between clock gating and devfreq scaling work
    commit 30fc33f1ef475480dc5bea4fe1bda84b003b992c upstream.
    
    UFS devfreq clock scaling work may require clocks to be ON if it need to
    execute some UFS commands hence it may request for clock hold before
    issuing the command. But if UFS clock gating work is already running in
    parallel, ungate work would end up waiting for the clock gating work to
    finish and as clock gating work would also wait for the clock scaling
    work to finish, we would enter in deadlock state. Here is the call trace
    during this deadlock state:
    
    Workqueue: devfreq_wq devfreq_monitor
    	__switch_to
    	__schedule
    	schedule
    	schedule_timeout
    	wait_for_common
    	wait_for_completion
    	flush_work
    	ufshcd_hold
    	ufshcd_send_uic_cmd
    	ufshcd_dme_get_attr
    	ufs_qcom_set_dme_vs_core_clk_ctrl_clear_div
    	ufs_qcom_clk_scale_notify
    	ufshcd_scale_clks
    	ufshcd_devfreq_target
    	update_devfreq
    	devfreq_monitor
    	process_one_work
    	worker_thread
    	kthread
    	ret_from_fork
    
    Workqueue: events ufshcd_gate_work
    	__switch_to
    	__schedule
    	schedule
    	schedule_preempt_disabled
    	__mutex_lock_slowpath
    	mutex_lock
    	devfreq_monitor_suspend
    	devfreq_simple_ondemand_handler
    	devfreq_suspend_device
    	ufshcd_gate_work
    	process_one_work
    	worker_thread
    	kthread
    	ret_from_fork
    
    Workqueue: events ufshcd_ungate_work
    	__switch_to
    	__schedule
    	schedule
    	schedule_timeout
    	wait_for_common
    	wait_for_completion
    	flush_work
    	__cancel_work_timer
    	cancel_delayed_work_sync
    	ufshcd_ungate_work
    	process_one_work
    	worker_thread
    	kthread
    	ret_from_fork
    
    This change fixes this deadlock by doing this in devfreq work (devfreq_wq):
    Try cancelling clock gating work. If we are able to cancel gating work
    or it wasn't scheduled, hold the clock reference count until scaling is
    in progress. If gate work is already running in parallel, let's skip
    the frequecy scaling at this time and it will be retried once next scaling
    window expires.
    
    Reviewed-by: Sahitya Tummala <stummala@codeaurora.org>
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -9503,19 +9503,13 @@ static void ufshcd_clk_scaling_resume_work(struct work_struct *work)
 static int ufshcd_devfreq_target(struct device *dev,
 				unsigned long *freq, u32 flags)
 {
-	int ret = 0;
+	int err = 0;
 	struct ufs_hba *hba = dev_get_drvdata(dev);
+	bool release_clk_hold = false;
 	unsigned long irq_flags;
-	ktime_t start;
-	bool scale_up, sched_clk_scaling_suspend_work = false;
 
-	if (!ufshcd_is_clkscaling_supported(hba))
+	if (!ufshcd_is_clkscaling_enabled(hba))
 		return -EINVAL;
-
-	if ((*freq > 0) && (*freq < UINT_MAX)) {
-		dev_err(hba->dev, "%s: invalid freq = %lu\n", __func__, *freq);
-		return -EINVAL;
-	}
 
 	spin_lock_irqsave(hba->host->host_lock, irq_flags);
 	if (ufshcd_eh_in_progress(hba)) {
@@ -9523,29 +9517,36 @@ static int ufshcd_devfreq_target(struct device *dev,
 		return 0;
 	}
 
-	if (!hba->clk_scaling.active_reqs)
-		sched_clk_scaling_suspend_work = true;
-
-	scale_up = (*freq == UINT_MAX) ? true : false;
-	if (!ufshcd_is_devfreq_scaling_required(hba, scale_up)) {
-		spin_unlock_irqrestore(hba->host->host_lock, irq_flags);
-		ret = 0;
-		goto out; /* no state change required */
+	if (ufshcd_is_clkgating_allowed(hba) &&
+	    (hba->clk_gating.state != CLKS_ON)) {
+		if (cancel_delayed_work(&hba->clk_gating.gate_work)) {
+			/* hold the vote until the scaling work is completed */
+			hba->clk_gating.active_reqs++;
+			release_clk_hold = true;
+			hba->clk_gating.state = CLKS_ON;
+		} else {
+			/*
+			 * Clock gating work seems to be running in parallel
+			 * hence skip scaling work to avoid deadlock between
+			 * current scaling work and gating work.
+			 */
+			spin_unlock_irqrestore(hba->host->host_lock, irq_flags);
+			return 0;
+		}
 	}
 	spin_unlock_irqrestore(hba->host->host_lock, irq_flags);
 
-	start = ktime_get();
-	ret = ufshcd_devfreq_scale(hba, scale_up);
-	trace_ufshcd_profile_clk_scaling(dev_name(hba->dev),
-		(scale_up ? "up" : "down"),
-		ktime_to_us(ktime_sub(ktime_get(), start)), ret);
+	if (*freq == UINT_MAX)
+		err = ufshcd_scale_clks(hba, true);
+	else if (*freq == 0)
+		err = ufshcd_scale_clks(hba, false);
 
-out:
-	if (sched_clk_scaling_suspend_work)
-		queue_work(hba->clk_scaling.workq,
-			   &hba->clk_scaling.suspend_work);
+	spin_lock_irqsave(hba->host->host_lock, irq_flags);
+	if (release_clk_hold)
+		__ufshcd_release(hba);
+	spin_unlock_irqrestore(hba->host->host_lock, irq_flags);
 
-	return ret;
+	return err;
 }
 
 static int ufshcd_devfreq_get_dev_status(struct device *dev,
