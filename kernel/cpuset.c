d8e62349889d716149eafe3f588c503d635f4482
Deokgyu Yang <secugyu@gmail.com>
2021-12-04 02:33:25 +0900

    kernel: cpuset: Remove duplicated cpuset_allow_attach function
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1456,24 +1456,6 @@ out_unlock:
 	return ret;
 }
 
-static int cpuset_allow_attach(struct cgroup_subsys_state *css,
-			       struct cgroup_taskset *tset)
-{
-	const struct cred *cred = current_cred(), *tcred;
-	struct task_struct *task;
-
-	cgroup_taskset_for_each(task, tset) {
-		tcred = __task_cred(task);
-
-		if ((current != task) && !capable(CAP_SYS_ADMIN) &&
-		    !uid_eq(cred->euid, tcred->uid) &&
-		    !uid_eq(cred->euid, tcred->suid))
-			return -EACCES;
-	}
-
-	return 0;
-}
-
 static void cpuset_cancel_attach(struct cgroup_subsys_state *css,
 				 struct cgroup_taskset *tset)
 {
